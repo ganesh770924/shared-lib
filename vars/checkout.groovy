a70f7250636d08c740b902a84d3d6d7f0aa28920
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2023-02-14 20:26:31 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,5 @@
 def call()
 {
-git branch: 'master', credentialsId: 'cicddemoaccesstoken', url: 'https://github.com/ganesh770924/CICDrepo.git'
+git branch: 'main', url: 'https://github.com/ganesh770924/CICDrepo.git'
 
 }
