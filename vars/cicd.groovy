110aa2e66ee49684c3b84f3be75d4c78b09f7d04
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2023-02-14 22:10:55 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,7 +13,7 @@ def sonarscanner(Closure body={}){
                  sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=CI_Jenkins \
                    -Dsonar.projectName=CI-CD_Jenkins \
                    -Dsonar.projectVersion=1.0 \
-                   -Dsonar.sources=src/ \
+                   -Dsonar.sources=src/ '''
                     
                }
 
