d74598022aecc66452e2d98bdeea05cb81685dc0
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2023-02-14 16:41:21 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,47 @@
+def checkout(Map Stageparams){
+git branch: 'master', credentialsId: 'cicddemoaccesstoken', url: 'https://github.com/ganesh770924/CICDrepo.git'
+}
+
+def build(){
+ sh "mvn clean install package -DskipTests"
+    }
+
+def test(){
+ junit(testResults: 'target/surefire-reports/*.xml', allowEmptyResults : true)
+}
+def sonarscanner(){
+                 sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=CI_Jenkins \
+                   -Dsonar.projectName=CI-CD_Jenkins \
+                   -Dsonar.projectVersion=1.0 \
+                   -Dsonar.sources=src/ \
+                   -Dsonar.java.binaries=/var/lib/jenkins/workspace/CI-CD/target/test-classes/com/example/demo '''
+               }
+
+def upartifacts(){
+  rtUpload (
+                    // Obtain an Artifactory server instance, defined in Jenkins --> Manage Jenkins --> Configure System:
+                    serverId: SERVER_ID,
+                    spec: """{
+                            "files": [
+                                    {
+                                        "pattern": "/var/lib/jenkins/workspace/CI-CD/target/*.war",
+                                        "target": "demo/"
+                                      
+                                    }
+                                ]
+                            }""",
+    buildName: '${env.BUILD_NUMBER}',
+    buildNumber: '${env.BUILD_TIMESTAMP}'
+                  )
+}
+
+def deploy(){
+   deploy adapters: 
+  [tomcat9(credentialsId: 'tomcat',
+ 
+   url: 'http://15.206.167.30:8080')],
+   contextPath: 'demo',
+   onFailure: false,
+   war: '**/target/*.war' 
+  
+}
