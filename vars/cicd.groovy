10e079996bfde2ce1292bc673d4d4a63fe1ab745
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2023-02-15 11:33:35 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -14,7 +14,7 @@ def sonarscanner(Closure body={}){
                    -Dsonar.projectName=CI-CD_Jenkins \
                    -Dsonar.projectVersion=1.0 \
                    -Dsonar.sources=src/
-                    -Dsonar.java.binaries=/var/jenkins_home/workspace/cicdpipeline/target/testclasses/com/dev3l/hello_world/test  '''
+                    -Dsonar.java.binaries=/var/jenkins_home/workspace/cicdpipeline/target/testclasses/com/dev3l/hello_world/test/ExampleTest.class '''
                     
                }
 
